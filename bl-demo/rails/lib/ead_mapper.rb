f5ffde967a0ddf38fd4e2a819c3299d03892f852
Bess Sadler <bess@ibiblio.org>
2009-04-28 17:39:41 -0400

    Now we have a rake task to index EAD files. This is a better system than having a separate command line script.

---
+++ bl-demo/rails/lib/ead_mapper.rb
@@ -0,0 +1,97 @@
+require 'rubygems'
+require 'xml/xslt'
+require 'find'
+require 'rsolr'
+require 'nokogiri'
+
+class EADMapper
+
+attr_reader :doc
+  
+  #pass in an EAD document
+  #set @ead_document
+  #kick off mapping method
+  def initialize(ead_file)
+    @xml = Nokogiri::XML(open(ead_file))
+    @doc = {}
+    self.getID
+    self.getFormatFacet
+    self.getTitle
+    self.getSubjects
+    self.getGeographicSubjects
+    self.getPublisherFacet
+    self.getGenreFormatFacet
+    self.getLanguageFacet
+    @doc
+  end
+  
+  def getFormatFacet
+    @doc[:format_facet] = "EAD"
+  end
+  
+  def getID
+    @xml.xpath('/ead/eadheader/eadid/@identifier').each do |id| 
+      @doc[:id] = id.content.gsub(/\//,"_").strip.gsub(/\s+/,"_").downcase
+    end
+  end
+
+  def getTitle
+    titles = []
+    @xml.xpath('/ead/eadheader/filedesc/titlestmt/titleproper[1]/text()').each_with_index do |title,i|
+      titles[i] = title.content.gsub(/\s+/," ")
+    end
+    @doc[:title_t] = titles.uniq
+  end
+  
+  def getSubjects
+    general_subjects = []
+    @xml.xpath('//subject/text()').each_with_index do |subject, i|
+      general_subjects[i] = subject.content.gsub(/\s+/," ")
+    end
+    
+    name_subjects = []
+    @xml.xpath('//persname|famname|corpname[@role="subject"]/text()').each_with_index do |subject, i|
+      name_subjects[i] = subject.content.gsub(/\s+/," ")
+    end
+
+    @doc[:subject_facet] = name_subjects.uniq.concat(general_subjects.uniq)
+  end
+  
+  def getGeographicSubjects
+    geographic_subject_facet = []
+    @xml.xpath('//geogname/text()').each_with_index do |subject, i|
+      geographic_subject_facet[i] = subject.content.gsub(/\s+/," ")
+    end
+    @doc[:geographic_subject_facet] = geographic_subject_facet.uniq
+  end
+  
+  def getLanguageFacet
+    language_facet = []
+    @xml.xpath('//language/text()').each_with_index do |language, i|
+      language_facet[i] = language.content.gsub(/\s+/," ").strip
+    end
+    @doc[:language_facet] = language_facet.uniq
+  end
+  
+  def getPublisherFacet
+    publisher_facet = []
+    @xml.xpath('/ead/eadheader/filedesc/publicationstmt/publisher/text()').each_with_index do |publisher, i|
+      publisher_facet[i] = publisher.content.gsub(/\s+/," ").strip
+    end
+    @doc[:publisher_facet] = publisher_facet.uniq
+  end
+  
+  def getGenreFormatFacet
+    genreform_facet = []
+    @xml.xpath('//genreform/text()').each_with_index do |form, i|
+      genreform_facet[i] = form.content.gsub(/\s+/," ").strip
+    end
+    @doc[:genreform_facet] = genreform_facet.uniq 
+  end
+  
+  def inspect
+    puts @doc.inspect
+  end
+  
+  
+end
--- bl-demo/rails/lib/tasks/app.rake
+++ bl-demo/rails/lib/tasks/app.rake
@@ -1,6 +1,15 @@
+require 'find'
+
+
 namespace :app do
   
   namespace :index do
+    
+    # *************************************************************** #
+    # Index marc files
+    # Note: solrmarc is probably better for production use, but this
+    # is a good place to start 
+    # *************************************************************** #
     
     desc 'Index a marc file at FILE=<location-of-file> using the lib/marc_mapper class.'
     task :marc => :environment do
@@ -26,6 +35,33 @@ namespace :app do
       
     end
     
+    # *************************************************************** #
+    # Index EAD files 
+    # *************************************************************** #
+    
+    desc 'Recursively index a directory of EAD files at DIR=<location-of-dir>'
+    task :ead => :environment do
+      t = Time.now
+      
+      ead_dir = ENV['DIR']
+      raise "Invalid directory. Set the directory by using the DIR argument." unless File.exists?(ead_dir.to_s) and File.directory?(ead_dir.to_s)
+      
+      solr = Blacklight.solr
+      
+      Find.find(ead_dir) do |path| 
+        if File.directory? path
+           puts "Indexing #{path}"
+        end
+        if File.file? path and path.to_s =~ /^.*\.xml$/
+          ead = EADMapper.new(path)
+          solr.add(ead.doc)
+        end
+      end
+      puts "Sending commit to Solr..."
+      solr.commit
+      puts "Complete."
+      puts "Total Time: #{Time.now - t}"
+    end
   end
   
 end
