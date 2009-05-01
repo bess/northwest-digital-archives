f9fc422f43ee1c8a0409605dc2ed0a35eac02670
Bess Sadler <bess@ibiblio.org>
2009-04-30 21:48:29 -0400

    I don't seem to be referencing my NWDA library properly, so solr_document.rb is breaking when I try to reference it. I'm commenting this out for now and I'll bring it back later when I have time to troubleshoot it.

--- nwda_blacklight/rails/app/models/solr_document.rb
+++ nwda_blacklight/rails/app/models/solr_document.rb
@@ -2,11 +2,11 @@ class SolrDocument
 
   include Blacklight::Solr::Doc
   include Blacklight::Solr::Doc::Ext::Common
-  include NWDA::Solr::Doc::Ext::Common
+  #include NWDA::Solr::Doc::Ext::Common
 
   after_initialize do
     # instance scope...
-    self.extend NWDA::Solr::Doc::Ext::EAD if ead?
+    #self.extend NWDA::Solr::Doc::Ext::EAD if ead?
   end
 
 end
