455ab003171d0ca87aec3dff651417acb8b1683f
Bess Sadler <bess@ibiblio.org>
2009-06-07 18:56:40 -0400

    added indexer for baseball collection

--- lib/nwda-lib/nwda/mappers.rb
+++ lib/nwda-lib/nwda/mappers.rb
@@ -2,6 +2,8 @@ module NWDA::Mappers
   
   autoload :Herbarium, 'nwda/mappers/herbarium.rb'
   autoload :Pullman, 'nwda/mappers/pullman.rb'
+  autoload :UW, 'nwda/mappers/uw.rb'
+  autoload :Baseball, 'nwda/mappers/baseball.rb'
   autoload :EAD, 'nwda/mappers/ead.rb'
   
 end
