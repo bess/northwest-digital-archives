5701a4a4407b2ad635b519de0bfa37cf5f027e3e
Bess Sadler <bess@ibiblio.org>
2009-06-14 11:34:37 -0400

    blacklight plugin installed

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,12 @@ RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
+require File.join(File.dirname(__FILE__), '../vendor/plugins/engines/boot')
 
 Rails::Initializer.run do |config|
+  config.gem 'authlogic'
+  config.plugin_paths += ["#{RAILS_ROOT}/vendor/plugins/blacklight/vendor/plugins"]
+  config.plugins = %W(engines blacklight acts_as_taggable_on_steroids resource_controller)
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
