f187060a29588db2600584c7adde63c4043b83e9
Matt Mitchell <goodieboy@gmail.com>
2009-06-30 22:47:37 -0400

    comments for the navigation method

--- lib/blacklight_solr_document_ead.rb
+++ lib/blacklight_solr_document_ead.rb
@@ -6,6 +6,12 @@ require_dependency 'blacklight/solr/document/ead.rb'
 
 class Blacklight::Solr::Document::EAD::Document
   
+  # Provides a navigation method to docs that mixin this module.
+  # This method returns a composite/tree object.
+  # It makes a query to solr using the :hierarchy_scope field as a filter,
+  # the values used is the value of THIS solr docs :hierarchy_scope value.
+  # The fl is set to "id,hierarchy" and also sets a very hi :rows value
+  # to ensure that all related docs are returned.
   def navigation
     @navigation ||= (
       res = Blacklight.solr.find({
