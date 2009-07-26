7b57263c4d4a4b49053c7f72383f34666c6fb5d7
Bess Sadler <bess@ibiblio.org>
2009-07-26 16:08:02 -0400

    Add a javascript snippet to move the EAD navigation element down on the screen so it appears next to the rest of the EAD document, per a request from Karen Estlund.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,12 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+
+// any div with class "below_appliedParams" gets a top margin == appliedParams.height()
+// this is to allow the EAD navigation to appear next to the actual EAD content, instead of floating
+// above it
+$(document).ready(function() {
+  // adds classes for zebra striping table rows
+  var h = $("#appliedParams").height();
+  $('div.below_appliedParams').attr("style","margin-top: " + h + "px");
+});
