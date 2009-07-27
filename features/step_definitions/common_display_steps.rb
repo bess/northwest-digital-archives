25807204131842724c949e84dd167903d090dac5
Bess Sadler <bess@ibiblio.org>
2009-07-27 19:53:51 -0400

    Add a step definition for applied filters

--- features/step_definitions/common_display_steps.rb
+++ features/step_definitions/common_display_steps.rb
@@ -1,5 +1,13 @@
 # Display features that all kinds of items should have
 
+
+Then /^I should see the applied filter "([^\"]*)" with the value "([^\"]*)"$/ do |filter, text|
+  response.should have_tag("div#facets div") do |node|
+    node.should have_selector("h3", :content => filter)
+    node.should have_selector("span.selected", :content => text)
+  end
+end
+
 Then /^I should see a title of "([^\"]*)"$/ do |title|
   response.should have_tag("h1", :text => title)
 end
