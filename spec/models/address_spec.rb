f69f1ad96d12fc522b689872aa0b7417e3cbfe46
Bess Sadler <bess@ibiblio.org>
2009-08-01 14:22:06 -0400

    More specs for validity of addresses

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -7,18 +7,29 @@ describe Address do
       :url => "http://www.foley.gonzaga.edu/spcoll/jopa.html"
       
     }
-    #@address = Address.new
+    @address = Address.create!(@valid_attributes)
   end
 
   it "should create a new instance given valid attributes" do
-    Address.create!(@valid_attributes)
+    a = Address.create!(@valid_attributes)
+    a.should be_valid
   end
 
-  # it "should be invalid without a name" do
-  #   @address.should_not_be_valid
-  #   @address.name = 'Foley Center Library'
-  #   @address.should_be_valid
-  # end
+  it "should be invalid without a name" do
+    @address.name = nil
+    @address.should_not be_valid
+    @address.errors.on(:name).should == "is required"
+    @address.name = 'Foley Center Library'
+    @address.should be_valid
+  end
+  
+  it "should be invalid without a url" do
+    @address.url = nil
+    @address.should_not be_valid
+    @address.errors.on(:url).should == "is required"
+    @address.url = 'http://www.foley.gonzaga.edu/spcoll/jopa.html'
+    @address.should be_valid
+  end
 
 
 end
