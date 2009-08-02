06921abae247b90143254d673e53adf96819b63d
Bess Sadler <bess@ibiblio.org>
2009-08-02 00:31:35 -0400

    street is not a required field, since not all archives seem to have street addresses

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,3 @@
 class Address < ActiveRecord::Base
-  validates_presence_of [:name, :institution, :url, :street, :city, :state, :postal_code, :phone_number], :message => 'is required'  
+  validates_presence_of [:name, :institution, :url, :city, :state, :postal_code, :phone_number], :message => 'is required'  
 end
