063bc50cbdcc9b817296c528eea9e4022e56245e
Bess Sadler <bess@ibiblio.org>
2009-08-01 21:14:54 -0400

    d'oh! I was accidentally ignoring database migrations.

---
+++ db/migrate/20090801174742_create_addresses.rb
@@ -0,0 +1,18 @@
+class CreateAddresses < ActiveRecord::Migration
+  def self.up
+    create_table :addresses do |t|
+      t.column :name, :string
+      t.column :url, :string
+      t.column :institution, :string
+      t.column :city, :string
+      t.column :state, :string
+      t.column :postal_code, :string
+      t.column :phone_number, :string
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :addresses
+  end
+end
